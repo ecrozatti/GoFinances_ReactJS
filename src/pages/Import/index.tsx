88e2878c9f1ed84248adbfb2261590d1220dcdb7
Eric Crozatti <eric.crozatti@gmail.com>
2020-07-01 23:09:55 -0300

    GoStack - Desafio 07

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -3,14 +3,14 @@ import { useHistory } from 'react-router-dom';
 
 import filesize from 'filesize';
 
+import alert from '../../assets/alert.svg';
+import api from '../../services/api';
+
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
 
 import { Container, Title, ImportFileContainer, Footer } from './styles';
-
-import alert from '../../assets/alert.svg';
-import api from '../../services/api';
 
 interface FileProps {
   file: File;
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(async file => {
+      // 'file' - it's the same name as the backend
+      data.append('file', file.file, file.name);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      try {
+        await api.post('/transactions/import', data);
+        history.push('/');
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const selectedFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles([...uploadedFiles, ...selectedFiles]);
   }
 
   return (
